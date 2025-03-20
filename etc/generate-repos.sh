30b2a846149e57723e47d32a7ce94324d8f31b36
Klaus Aehlig <klaus.aehlig@huawei.com>
2025-03-20 15:54:05 +0100

    Update just reference to 1.5.0

--- etc/generate-repos.sh
+++ etc/generate-repos.sh
@@ -17,7 +17,7 @@ set -eu
 
 readonly ROOT=$(readlink -f $(dirname $0)/..)
 
-: ${BRANCH:=v1.4.0}
+: ${BRANCH:=v1.5.0}
 : ${OUTFILE:=${ROOT}/etc/repos.json}
 
 just-import-git --as just -C ${ROOT}/etc/repos.template.json \
