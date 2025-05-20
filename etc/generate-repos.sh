6d428dc387ac9d6d5e744c8268840846e9d9951d
Klaus Aehlig <klaus.aehlig@huawei.com>
2025-05-20 10:08:40 +0200

    Update just reference
    ... after the grpc update.

--- etc/generate-repos.sh
+++ etc/generate-repos.sh
@@ -17,7 +17,7 @@ set -eu
 
 readonly ROOT=$(readlink -f $(dirname $0)/..)
 
-: ${BRANCH:=v1.5.0}
+: ${BRANCH:=v1.5.2}
 : ${OUTFILE:=${ROOT}/etc/repos.json}
 
 just-import-git --as just -C ${ROOT}/etc/repos.template.json \
